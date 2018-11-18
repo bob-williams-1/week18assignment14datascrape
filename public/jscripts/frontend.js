796e9cc9adc9fe7f158e7776cf27f9a91ab34b00
bob-williams-1 <bob.williams.dev@gmail.com>
2018-11-18 11:29:14 -0500

    data scrape 3

--- public/jscripts/frontend.js
+++ public/jscripts/frontend.js
@@ -3,16 +3,17 @@ $(document).ready(function(){
   $("button").click(function(e){
     $.getJSON("/" + $(this).val(), function(response) {
       $("tbody").empty();
-      response.forEach(function(animal) {
+      response.forEach(function(person) {
         var newTr = "<tr>";
-        newTr += "<td>" + animal.title + "</td>";
-        newTr += "<td>" + animal.link + "</td>";
+        newTr += "<td>" + person.title + "</td>";
+        newTr += "<td>" + person.link + "</td>";
         newTr += "</tr>";
         $("tbody").append(newTr);
       });
     });
   });
 
+  
   $("#nameButton").click(function(e) {
     alert("Clicked name!")
   });
